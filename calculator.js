5d8ca0489f51987e1e3c2b361ad4a0896db4e268
Marcos Vazquez <mmvazquez364@gmail.com>
2025-09-27 22:54:29 -0400

    Handled division by zero and display limit; fixed bug with resetting display

--- calculator.js
+++ calculator.js
@@ -13,7 +13,7 @@ function multiply(x, y) {
 function divide(x, y) {
     if (y === 0) {
         alert("CAN'T DO DIVISION BY 0");
-        return;
+        return "NaN";
     }
     return String(x / y);
 }
@@ -29,23 +29,31 @@ let operand1 = "";
 let operator = "";
 let operand2 = "";
 
+let resetDisplay = false;
+
 const display = document.querySelector(".display");
 display.textContent = "0";
 const btns = document.querySelector(".buttons-container");
 console.log(btns);
 btns.addEventListener("click", (e) => {
     let target = e.target;
-
     if (target.id === "clear") {
         display.textContent = "0";
         operand1 = "";
         operand2 = "";
+        return;
+    }
 
-    } else if (Number.isInteger(parseInt(target.id)) && display.textContent.length < 8) {        
-        if (display.textContent === "0" || 
-            display.textContent === operand1 || display.textContent === operand2) {
+    if (display.textContent === "NaN") {
+        // prevents user from interacting with calculator until "clear" is clicked
+        return;
+    }
+    
+    if (Number.isInteger(parseInt(target.id))) {      
+        if (display.textContent === "0" || resetDisplay) {
             display.textContent = target.id;
-        } else {
+            resetDisplay = false;
+        } else if (display.textContent.length < 12) {
             display.textContent += target.id;
         }
 
@@ -53,29 +61,34 @@ btns.addEventListener("click", (e) => {
         operand2 = operand1.length !== 0 ? display.textContent : "";
         operand1 = operand2.length === 0 ? display.textContent : operate(operand1, operand2, operator);
         display.textContent = operand1;
+        resetDisplay = true;
         operator = "+";
 
     } else if (target.id === "subtract") {
         operand2 = operand1.length !== 0 ? display.textContent : "";
         operand1 = operand2.length === 0 ? display.textContent : operate(operand1, operand2, operator);
         display.textContent = operand1;
+        resetDisplay = true;
         operator = "-";
 
     } else if (target.id === "multiply") {
         operand2 = operand1.length !== 0 ? display.textContent : "";
         operand1 = operand2.length === 0 ? display.textContent : operate(operand1, operand2, operator);
         display.textContent = operand1;
+        resetDisplay = true;
         operator = "*";
 
     } else if (target.id === "divide") {
         operand2 = operand1.length !== 0 ? display.textContent : "";
         operand1 = operand2.length === 0 ? display.textContent : operate(operand1, operand2, operator);
         display.textContent = operand1;
+        resetDisplay = true;
         operator = "/";
 
     } else if (target.id === "equals") {
         display.textContent = operate(operand1, display.textContent, operator);
         operand1 = "";
-        operand2 = display.textContent;
+        operand2 = "";
+        resetDisplay = true;
     }
 });
